c2daf792d1939503b4c47f854ef570f4210423a4
Melissa <chopinetmelissa@live.fr>
2023-05-15 19:37:45 +0200

    Ajout couleur au clic sur filtre

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -204,7 +204,7 @@
         '<li class="nav-item"><span class="nav-link active active-tag"  data-images-toggle="all">Tous</span></li>';
       $.each(tags, function (index, value) {
         tagItems += `<li class="nav-item active">
-                <span class="nav-link"  data-images-toggle="${value}">${value}</span></li>`;
+                <span class="nav-link "  data-images-toggle="${value}">${value}</span></li>`;
       });
       var tagsRow = `<ul class="my-4 tags-bar nav nav-pills">${tagItems}</ul>`;
       if (position === "bottom") {
@@ -220,7 +220,7 @@
         return;
       }
       $(".active-tag").removeClass("active active-tag");
-      $(this).addClass("active-tag");
+      $(this).addClass("active active-tag");
       var tag = $(this).data("images-toggle");
       $(".gallery-item").each(function () {
         $(this).parents(".item-column").hide();
